f694b06d43b81ce7dee2a9dbc8389c1e1045219f
mertcakmakk <mert.cakmak@getir.com>
2024-01-27 12:54:47 +0300

    spring-k8s-argocd

---
+++ src/main/java/com/example/springargocdapp/SpringArgoCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springargocdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArgoCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringArgoCdApplication.class, args);
+    }
+
+}
